cf7fedd4b6699571dc7159c7efe1f4a74335e7a3
Ferael1 <00005620@uca.edu.sv>
2021-03-14 16:32:36 -0600

    Solved merge conflicts with TaskB

--- Github/Program.cs
+++ Github/Program.cs
@@ -14,21 +14,13 @@ namespace Github
             Console.WriteLine("Enter a value for b:");
             b = Int32.Parse(Console.ReadLine());
 
-<<<<<<< HEAD
-            Console.WriteLine($"Substracting {a} + {b} equals {subsNumbers(a,b)}");
-        }
 
-        public static int subsNumbers(int a, int b)
-        {
-            return a-b;
-=======
             Console.WriteLine($"Multiplying {a} x {b} equals {multNumbers(a,b)}");
         }
 
         public static int multNumbers(int a, int b)
         {
             return a*b;
->>>>>>> TaskB
         }
     }
 }
